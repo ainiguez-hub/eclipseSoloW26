62d2fe1f81584351f0809753e4ee8eeff55df70b
ainiguez-hub <ainiguez@gmail.com>
2026-02-17 15:58:24 -0500

    added some more stuff

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -3,5 +3,7 @@ package eclipseSoloProjA;
 public class HelloWorld{
 		public static void main (String [] args) {
 			System.out.println("github Project number two");
+			System.out.println("second change");
+
 		}
 }
