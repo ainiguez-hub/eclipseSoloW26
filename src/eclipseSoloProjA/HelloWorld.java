57e76ac5c697d822ecd723fa31482133774efe8e
ainiguez-hub <ainiguez@gmail.com>
2026-02-17 17:01:27 -0500

    New additions

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld{
 		public static void main (String [] args) {
 			System.out.println("github Project number two");
 			System.out.println("second change");
+			System.out.println("programer 2 did this");
+
 
 		}
 }
