30773056ff769ed7b0646b84041d24607bcfb2e8
ainiguez-hub <ainiguez@gmail.com>
2026-02-17 23:25:19 -0500

    mog

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld{
 		public static void main (String [] args) {
 			System.out.println("github Project number two");
 			System.out.println("second change");
-			System.out.println("programer 2 did this programer one time did this");
+			System.out.println("programer 2 did this programer one lmao time did this");
 
 		}
 }
